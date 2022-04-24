d9ab41930edcb3c3acda6c25269e47344f4f7073
robb <gxrobb@gmail.com>
2022-04-23 22:43:16 -0400

    api call added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="home">
-
+  <div class="main">
+    <pre>
+      {{ data }}      
+    </pre>
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import axios from 'axios'
+import { defineComponent, ref } from 'vue';
 // import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
@@ -14,5 +17,18 @@ export default defineComponent({
   components: {
     // HelloWorld,
   },
+  setup(){
+    const API_BASE = process.env.VUE_APP_SPACE_FARMERS_API_PUBLIC_BASE
+    const data = ref(Object)
+    return { API_BASE, data }
+  },
+  mounted() {
+    this.getData()
+  },
+  methods: {
+    async getData(){
+      this.data = await axios.get(this.API_BASE+'/farmers')
+    }
+  }
 });
 </script>
