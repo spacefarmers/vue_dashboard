391aed605777e0233268fd60a6dc0f14608f8e4e
robb <gxrobb@gmail.com>
2022-04-27 17:01:05 -0400

    updated variables

--- src/App.vue
+++ src/App.vue
@@ -18,24 +18,18 @@ import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
 
-  setup(){
-    const theme = ref('')
-    const targetTheme = ref('')
+  // setup(){
+  //   const theme = ref('')
+  //   const targetTheme = ref('')
 
-    return {theme, targetTheme}
-  },
+  //   return {theme, targetTheme}
+  // },
 
   created(){
 
     if(!localStorage.getItem('theme')) {
       localStorage.setItem('theme', 'dark')
     }
-
-    // this.theme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : '');
-
-    // if(this.theme) {
-    //   document.documentElement.setAttribute('data-theme', this.theme)
-    // }
 
   },
   methods: {
@@ -57,19 +51,47 @@ export default defineComponent({
 
 <style lang="scss">
 :root {
-  /* Your default theme */
-  --background: white;
-  --foreground: black;
+  // default theme
+  --background: #1A1D21;
+  --body-color: white;
+  --gray: #A8A8A8;
+  --gray-text: #A8A8A8;
+  --light-gray: #CED4D4;
+  --light-gray-text: #CED4D4;
+  --success: #13C54B;
+  --warning: #E8BC52;
+  --info: #2886F6;
+  --card-background-color: #212529;
+  --white: white;
 }
 [data-theme='light'] {
-  --background: black;
-  --foreground: white;
+  --background: white;
+  --body-color: #212529;
+  --gray: #A8A8A8;
+  --gray-text: #A8A8A8;
+  --light-gray: #CED4D4;
+  --light-gray-text: #CED4D4;
+  --success: #13C54B;
+  --warning: #E8BC52;
+  --info: #2886F6;
+  --card-background-color: #212529;
+  --white: white;
 }
 [data-theme='dark'] {
-  --background: green;
-  --foreground: white;
+  --background: #1A1D21;
+  --body-color: white;
+  --gray: #212529;
+  --gray-text: #212529;
+  --light-gray: #FCFCFC;
+  --light-gray-text: #808D9B;
+  --success: #13C54B;
+  --warning: #E8BC52;
+  --info: #2886F6;
+  --card-background-color: #FCFCFC;
+  --white: white;
 }
 body{
-  background: var(--background);
+  background-color: var(--background);
+  color: var(--body-color);
 }
 </style>
